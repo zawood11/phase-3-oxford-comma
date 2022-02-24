4bcb71623e9e843380d7a1b7440c670e1528cb62
Zach <zawood11@gmail.com>
2022-02-24 17:43:14 -0500

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
